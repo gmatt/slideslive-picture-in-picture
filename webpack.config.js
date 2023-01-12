61e90e0ba01e1e9c2c45748d4d9718878bd7fed1
Matt Sárdi <sardi.matt@gmail.com>
2023-01-12 22:06:15 +0100

    chore: configure bookmarklet-webpack-plugin based on readme
    https://github.com/tomrule007/bookmarklet-webpack-plugin/blob/6f5fd2c3f4ee131c00f4cb8275a28965cc4deace/README.md

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,16 @@
+const BookmarkletPlugin = require("bookmarklet-webpack-plugin");
 const path = require("path");
 
 module.exports = {
+    mode: "production",
     entry: "./src/index.js",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist"),
     },
+    plugins: [
+        new BookmarkletPlugin({
+            input: "main.js",
+        }),
+    ],
 };
