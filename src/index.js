bf6a6ccc52c6f60ce9d43f0ca723db027ed061d6
Matt Sárdi <sardi.matt@gmail.com>
2023-01-14 18:07:23 +0100

    refactor: wrap code inside a function
    So I can test it quicker in the console.

--- src/index.js
+++ src/index.js
@@ -1,107 +1,113 @@
 // @ts-check
 
-/** @type {() => HTMLVideoElement} */
-const getVideo = () => {
-    return document.getElementsByTagName("video")[0];
-};
+(() => {
+    /** @type {() => HTMLVideoElement} */
+    const getVideo = () => {
+        return document.getElementsByTagName("video")[0];
+    };
 
-/** @type {() => HTMLImageElement} */
-const getSlideImage = () => {
-    const element = document.querySelector(".slp__slidesPlayer__content img");
-    if (!(element instanceof HTMLImageElement)) {
-        throw Error("Slide image not found.");
-    }
-    return element;
-};
+    /** @type {() => HTMLImageElement} */
+    const getSlideImage = () => {
+        const element = document.querySelector(
+            ".slp__slidesPlayer__content img",
+        );
+        if (!(element instanceof HTMLImageElement)) {
+            throw Error("Slide image not found.");
+        }
+        return element;
+    };
 
-const originalVideo = getVideo();
-// TODO Check if needed.
-originalVideo.crossOrigin = "anonymous";
-
-// We copy the main video. This is a way to get around CORS limitations.
-const addedVideo = document.createElement("video");
-addedVideo.src = originalVideo.src;
-addedVideo.crossOrigin = "anonymous";
-document.body.appendChild(addedVideo);
-addedVideo.muted = true;
-addedVideo.play();
-addedVideo.width = originalVideo.width;
-
-const slideImage = getSlideImage();
-
-const canvas = document.createElement("canvas");
-canvas.width = originalVideo.clientWidth + slideImage.width;
-canvas.height = Math.max(originalVideo.clientHeight, slideImage.height);
-// For retina.
-canvas.width *= window.devicePixelRatio;
-canvas.height *= window.devicePixelRatio;
-
-document.body.appendChild(canvas);
-
-const outputVideo = document.createElement("video");
-// TODO Check if needed.
-outputVideo.width = 640;
-outputVideo.width = 480;
-outputVideo.playsInline = true;
-outputVideo.autoplay = true;
-// videoOutput.muted = true;
-
-document.body.appendChild(outputVideo);
-
-outputVideo.srcObject = canvas.captureStream();
-
-const ctx = canvas.getContext("2d");
-if (!ctx) {
-    throw Error("Can't get canvas context.");
-}
-// Set resolution for retina screens.
-ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
-
-const mainLoop = () => {
-    // Sync the timestamp of the original video with the added video.
-    if (Math.abs(addedVideo.currentTime - originalVideo.currentTime) > 0.5) {
-        addedVideo.currentTime = originalVideo.currentTime;
-    }
-
-    // var video = document.getElementsByTagName("video")[0];
-    // ctx.drawImage(video, 0, 0, video.clientWidth, video.clientHeight);
-
-    ctx.drawImage(
-        addedVideo,
-        0,
-        0,
-        originalVideo.clientWidth,
-        originalVideo.clientHeight,
-    );
-
-    /** @type {HTMLImageElement} */
+    const originalVideo = getVideo();
     // TODO Check if needed.
+    originalVideo.crossOrigin = "anonymous";
+
+    // We copy the main video. This is a way to get around CORS limitations.
+    const addedVideo = document.createElement("video");
+    addedVideo.src = originalVideo.src;
+    addedVideo.crossOrigin = "anonymous";
+    document.body.appendChild(addedVideo);
+    addedVideo.muted = true;
+    addedVideo.play();
+    addedVideo.width = originalVideo.width;
+
     const slideImage = getSlideImage();
 
-    ctx.drawImage(
-        slideImage,
-        originalVideo.clientWidth,
-        0,
-        slideImage.width,
-        slideImage.height,
-    );
+    const canvas = document.createElement("canvas");
+    canvas.width = originalVideo.clientWidth + slideImage.width;
+    canvas.height = Math.max(originalVideo.clientHeight, slideImage.height);
+    // For retina.
+    canvas.width *= window.devicePixelRatio;
+    canvas.height *= window.devicePixelRatio;
 
-    // requestAnimationFrame(loop);
-    setTimeout(mainLoop);
-};
-// requestAnimationFrame(loop);
-mainLoop();
+    document.body.appendChild(canvas);
 
-// Add 'pip' button.
-const button = document.createElement("button");
-button.textContent = "pip";
-document.body.appendChild(button);
-button.addEventListener("click", async () => {
-    if (document.pictureInPictureEnabled) {
-        outputVideo.requestPictureInPicture();
-    } else {
-        // TODO Check if needed for Safari.
-        outputVideo.play();
-        outputVideo.webkitSetPresentationMode("picture-in-picture");
+    const outputVideo = document.createElement("video");
+    // TODO Check if needed.
+    outputVideo.width = 640;
+    outputVideo.width = 480;
+    outputVideo.playsInline = true;
+    outputVideo.autoplay = true;
+    // videoOutput.muted = true;
+
+    document.body.appendChild(outputVideo);
+
+    outputVideo.srcObject = canvas.captureStream();
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) {
+        throw Error("Can't get canvas context.");
     }
-});
+    // Set resolution for retina screens.
+    ctx.scale(window.devicePixelRatio, window.devicePixelRatio);
+
+    const mainLoop = () => {
+        // Sync the timestamp of the original video with the added video.
+        if (
+            Math.abs(addedVideo.currentTime - originalVideo.currentTime) > 0.5
+        ) {
+            addedVideo.currentTime = originalVideo.currentTime;
+        }
+
+        // var video = document.getElementsByTagName("video")[0];
+        // ctx.drawImage(video, 0, 0, video.clientWidth, video.clientHeight);
+
+        ctx.drawImage(
+            addedVideo,
+            0,
+            0,
+            originalVideo.clientWidth,
+            originalVideo.clientHeight,
+        );
+
+        /** @type {HTMLImageElement} */
+        // TODO Check if needed.
+        const slideImage = getSlideImage();
+
+        ctx.drawImage(
+            slideImage,
+            originalVideo.clientWidth,
+            0,
+            slideImage.width,
+            slideImage.height,
+        );
+
+        // requestAnimationFrame(loop);
+        setTimeout(mainLoop);
+    };
+    // requestAnimationFrame(loop);
+    mainLoop();
+
+    // Add 'pip' button.
+    const button = document.createElement("button");
+    button.textContent = "pip";
+    document.body.appendChild(button);
+    button.addEventListener("click", async () => {
+        if (document.pictureInPictureEnabled) {
+            outputVideo.requestPictureInPicture();
+        } else {
+            // TODO Check if needed for Safari.
+            outputVideo.play();
+            outputVideo.webkitSetPresentationMode("picture-in-picture");
+        }
+    });
+})();
